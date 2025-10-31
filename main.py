a85968d24070a98ead6c72f30d6c31b382bd7088
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2025-10-31 22:27:54 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,79 @@
+import random
+content=list()
+list_of_commands = ['print', 'math', 'list', 'random_number', 'append_in_list']
+math_list = ['+',"-","*","/"]
+letters = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
+list1, list2, list3= list(), list(), list()
+def W_1(command):
+    if command == 'print':
+        print(''.join(random.choices(letters, k=random.randint(3, 8))))
+    elif command == 'math':
+        randg_num1=random.randint(-999, 999)
+        randg_num2=random.randint(-999, 999)
+        if random.choice(math_list) == '+':
+            print(f'{randg_num1} + {randg_num2} = {randg_num1+randg_num2}')
+        elif random.choice(math_list) == '-':
+            print(f'{randg_num1} - {randg_num2} = {randg_num1-randg_num2}')
+        elif random.choice(math_list) == '*':
+            print(f'{randg_num1} * {randg_num2} = {randg_num1*randg_num2}')
+        elif random.choice(math_list):
+            try:
+                print(f'{randg_num1} / {randg_num2} = {randg_num1/randg_num2}')
+            except ZeroDivisionError:
+                print('ZeroDivisionError')
+    elif command == 'list':
+        if random.randint(1, 3) == 1:
+            print(f'List1: {list1}')
+        elif random.randint(1, 3) == 2:
+            print(f'List2: {list2}')
+        elif random.randint(1, 3) == 3:
+            print(f'List3: {list3}')
+        else:
+            print("List id ")
+    elif command == 'random_number':
+        number_generated_randomly=random.randint(-999, 999)
+        print(number_generated_randomly)
+    elif command == 'append_in_list':
+        if random.randint(1, 2) == 1:
+            if random.randint(1, 3) == 1:
+                list1.append(random.randint(-999, 999))
+            elif random.randint(1, 3) == 2:
+                list2.append(random.randint(-999, 999))
+            elif random.randint(1, 3) == 3:
+                list3.append(random.randint(-999, 999))
+        elif random.randint(1, 2) == 2:
+            if random.randint(1, 3) == 1:
+                list1.append(''.join(random.choices(letters, k=random.randint(3, 8))))
+            elif random.randint(1, 3) == 2:
+                list2.append(''.join(random.choices(letters, k=random.randint(3, 8))))
+            elif random.randint(1, 3) == 3:
+                list3.append(''.join(random.choices(letters, k=random.randint(3, 8))))
+
+def READ_W(filename):
+    with open(filename, 'r') as file:
+        commands = file.readlines()
+    return [command_1.strip() for command_1 in commands]
+
+def EXEC_W(filename):
+    commands = READ_W(filename)
+    for command in commands:
+            if command in list_of_commands:
+                W_1(command)
+            elif command.startswith('W(') and command.endswith(')'):
+                try:
+                    num_commands = int(command[2:-1])
+                    W(num_commands)
+                except ValueError:
+                    print(f"ValueError: {command}")
+            else:
+                print(f'Unkown command: {command}')
+
+def W(code_lines):
+    try:
+        for i in range(code_lines):
+            current_command=random.choice(list_of_commands)
+            content.append(current_command)
+            W_1(current_command)
+    except:
+        print("Error creating loop.")
+EXEC_W("test.w") # Replace test.w with your file path
